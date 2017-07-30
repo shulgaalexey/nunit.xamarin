7756c5e3bdfe327bd892aa8c761f74f8e0c17b83
shulga <a.shulga@samsung.com>
2017-07-31 08:35:20 +0900

    Updating Tizen Runner project settings
    Signed-off-by: shulga <a.shulga@samsung.com>

---
+++ src/runner/nunit.runner.Tizen/nunit.runner.Tizen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nunit.runner.Tizen
+{
+    public class MyLibrary
+    {
+        public string MyMethod()
+        {
+            return "Hello Tizen Library!!!";
+        }
+    }
+}
